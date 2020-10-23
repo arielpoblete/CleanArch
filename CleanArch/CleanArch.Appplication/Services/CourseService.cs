2535532879af250914f045152e7b3f11dcc1903d
apobletec <apobletec@hogardecristo.cl>
2020-10-23 16:37:34 -0300

    IMplemtation Services and Repository

---
+++ CleanArch/CleanArch.Appplication/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Appplication.Interfaces;
+using CleanArch.Appplication.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Appplication.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        CourseViewModel ICourseService.GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Data/Repository/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Data.Context;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            _ctx = universityDBContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
