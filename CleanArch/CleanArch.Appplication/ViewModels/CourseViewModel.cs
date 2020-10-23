21ef8264e19cfac37c0c8c82125ab045960f3463
apobletec <apobletec@hogardecristo.cl>
2020-10-23 15:34:21 -0300

    Application layer

---
+++ CleanArch/CleanArch.Appplication/ViewModels/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domain.Models;
+using System.Collections.Generic;
+
+namespace CleanArch.Appplication.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
